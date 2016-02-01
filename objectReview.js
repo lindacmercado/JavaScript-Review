6a0a46cb886d2713ddee9c713d890c1da1c2fdf9
Linda Mercado <lindacmercado@gmail.com>
2016-01-31 20:38:40 -0700

    complete

--- objectReview.js
+++ objectReview.js
@@ -1,23 +1,34 @@
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. 
+//Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
+ var favoriteThings = {
+     band: 'Journey',
+     food: 'Chocolate',
+     person: 'Wonder Woman',
+     book: 'Pride and Prejudice',
+     movie: 'Star Wars',
+     holiday: 'Christmas'
+ }
 
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+//After you've made your object, add another key named 'car' with the value being your favorite car and then 
+//another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+ favoriteThings.car = 'Jeep'; 
+ favoriteThings.brand = 'Apple';
 
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your 
+//favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades of Gray';
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
-
+alert(favoriteThings.person);
+alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
 
 
 var user = {
@@ -29,17 +40,26 @@ var user = {
     age: 0
 }
 
-//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
+//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. 
+//If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
+for(var key in user) {
+    if(!user[key]) {
+        delete user[key];
+    }  
+}
 
-//Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Your code here
+//Once you get your truthy object, Change the remaining values in the object to be specific to you 
+//(name: 'your name', username: 'your username'), rather than my information.
+
+user.name = 'Linda Mercado';
+user.pwHash = '123456';
+user.username = 'teena72';
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
+console.log(user);
 
 
 
@@ -49,15 +69,22 @@ var user = {
 
 //create an empty object called methodCollection. 
 
-  //Your code here
+var methodCollection = {};
 
-//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
+//Now add two methods (functions that are properties on objects) to your methodCollection object. 
+//One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
-  //Your code here
+methodCollection.alertHello = function() {
+    alert('hello');
+}
+methodCollection.logHello = function() {
+    console.log('hello');
+}
 
 //Now call your alertHello and logHello methods. 
 
-  //Your code here
+ methodCollection.alertHello();
+ methodCollection.logHello();
 
 
 
@@ -65,8 +92,26 @@ var user = {
 
 
 
-//write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
+//write a function called voweler that accepts a string, and returns an object with the keys being all the 
+//vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
 
-
-
+function vowler(inputStr) {
+    var vowels = {
+	a: 0,
+	e: 0,
+	i: 0,
+	o: 0,
+	u: 0
+    };
+    var newStr = inputStr.split('');
+    for (var i = 0; i < newStr.length; i++) {
+	for (var key in vowels) {
+	    if (newStr[i] === key) {
+		vowels[key] += 1;
+	    }
+	}
+    }
+    return vowels;  
+}
+vowler('This is a test sentence');
